226f9f399072b13160232a3ebc58206291b1de43
Yehor Stopkin <yehorStopkin@gmail.com>
2021-12-26 17:03:31 +0100

    Added font-awesome Added base styling for top-bar

--- src/components/Common/TopBar/TopBar.component.tsx
+++ src/components/Common/TopBar/TopBar.component.tsx
@@ -1,26 +1,31 @@
 import './TopBar.component.css';
 
 function TopBarComponent(props: any) {
+
     return (
         <div className="top-bar">
-            <div>
-                Ссылка на страницу категорий
+            <div className="top-bar__left">
+                <div className="top-bar__categories top-bar__action">
+                    Ссылка на страницу категорий
+                </div>
             </div>
 
-            <div>
-                Картинка корзины со счётчиком товаров
-            </div>
+            <div className="top-bar__right">
+                <div className="top-bar__cart top-bar__action" >
+                    <i className="fa fa-shopping-cart"></i>
+                </div>
 
-            <div>
-                Кнопка логина {/** Если пользователь не авторизирован */}
-            </div>
+                <div className="top-bar__login top-bar__action top-bar__button">
+                    Кнопка логина {/** Если пользователь не авторизирован */}
+                </div>
 
-            <div>
-                Ссылка на личный кабинет {/**Если пользователь авторизирован */}
-            </div>
+                <div className="top-bar__personal-cabinet top-bar__action">
+                    Ссылка на личный кабинет {/**Если пользователь авторизирован */}
+                </div>
 
-            <div>
-                Кнопка логаута {/** Если пользователь авторизирован */}
+                <div className="top-bar__logout top-bar__action top-bar__button">
+                    Кнопка логаута {/** Если пользователь авторизирован */}
+                </div>
             </div>
         </div>
     )
