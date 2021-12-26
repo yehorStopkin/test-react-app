429d32df090793ecf48071e0f931e9a590fae809
Yehor Stopkin <yehorStopkin@gmail.com>
2021-12-26 23:07:30 +0100

    Added top-bar

--- src/components/Common/TopBar/TopBar.component.tsx
+++ src/components/Common/TopBar/TopBar.component.tsx
@@ -1,31 +1,78 @@
+import React from 'react';
 import './TopBar.component.css';
 
 function TopBarComponent(props: any) {
+    const [needLogin, setNeedLogin] = React.useState<boolean>(false);
+
+    function login() {
+        setNeedLogin(false);
+    }
+
+    function logout() {
+        setNeedLogin(true);
+    }
+
+    function openPersonalCabinet() {
+        alert('Personal cabinet is opened!');
+    }
+
+    function openCart() {
+        alert('Shopping cart is opened!');
+    }
+
+    function openCategories() {
+        alert('Page with categories is opened!');
+    }
 
     return (
         <div className="top-bar">
             <div className="top-bar__left">
-                <div className="top-bar__categories top-bar__action">
-                    Ссылка на страницу категорий
+                <div
+                    onClick={() => openCategories()}
+                    className="top-bar__categories top-bar__action"
+                >
+                    <i className="fa fa-list" /> 
+                    <span> Categories </span>
                 </div>
             </div>
 
             <div className="top-bar__right">
-                <div className="top-bar__cart top-bar__action" >
+                {needLogin ? null :
+                    <div
+                        onClick={() => openPersonalCabinet()}
+                        className="top-bar__personal-cabinet top-bar__action"
+                        title="Personal cabinet"
+                    >
+                        <i className="fa fa-user" />
+                    </div>
+                }
+
+                <div
+                    onClick={() => openCart()}
+                    className="top-bar__cart top-bar__action"
+                >
                     <i className="fa fa-shopping-cart"></i>
                 </div>
 
-                <div className="top-bar__login top-bar__action top-bar__button">
-                    Кнопка логина {/** Если пользователь не авторизирован */}
-                </div>
+                {needLogin ?
+                    <div
+                        onClick={() => login()}
+                        className="top-bar__login top-bar__action"
+                        title="Login"
+                    >
+                        <i className="fa fa-sign-in" />
+                    </div>
+                    :
+                    <div
+                        onClick={() => logout()}
+                        className="top-bar__logout top-bar__action"
+                        title="Logout"
+                    >
+                        <i className="fa fa-sign-out" />
+                    </div>
+                }
 
-                <div className="top-bar__personal-cabinet top-bar__action">
-                    Ссылка на личный кабинет {/**Если пользователь авторизирован */}
-                </div>
 
-                <div className="top-bar__logout top-bar__action top-bar__button">
-                    Кнопка логаута {/** Если пользователь авторизирован */}
-                </div>
             </div>
         </div>
     )
